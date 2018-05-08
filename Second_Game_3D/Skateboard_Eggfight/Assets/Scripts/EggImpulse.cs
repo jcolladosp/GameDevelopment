b58b4388fe75425cf725e8a695afe8db01aaf01c
Jose Collado San Pedro <jocolsan@inf.upv.es>
2018-05-08 16:40:40 +0100

    new

---
+++ Second_Game_3D/Skateboard_Eggfight/Assets/Scripts/EggImpulse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggImpulse : MonoBehaviour {
+	public float impulseForce = 15.0f;
+	public float impulseForceUp = 3.0f;
+
+
+	private GameObject eggHand;
+
+	// Use this for initialization
+	void Start () {
+		eggHand = GameObject.Find("eggHand");
+		GetComponent<Rigidbody> ().AddRelativeForce ((-eggHand.transform.forward) * impulseForce, ForceMode.Impulse);
+		GetComponent<Rigidbody> ().AddRelativeForce ((eggHand.transform.up) * impulseForceUp, ForceMode.Impulse);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < 0.0f) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Second_Game_3D/Skateboard_Eggfight/Assets/Scripts/Skater.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skater : MonoBehaviour {
+	private bool isInsideTable = false;
+	private bool hasEgg = false;
+	public GameObject egg;
+	public GameObject eggInstancy;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.E) && isInsideTable && !hasEgg)
+		{
+			hasEgg = true;
+			egg.GetComponent<MeshRenderer> ().enabled = true;
+
+		}
+		if (Input.GetKey (KeyCode.Mouse0) && hasEgg) {
+
+			Instantiate (eggInstancy, egg.transform.position, Quaternion.identity);
+			hasEgg = false;
+			egg.GetComponent<MeshRenderer> ().enabled = false;
+
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		isInsideTable = true;
+	}
+	void OnTriggerExit(Collider other){
+		isInsideTable = false;
+	}
+}
